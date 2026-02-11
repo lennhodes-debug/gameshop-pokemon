4bcc89bf6cc546b7a5ce021a40e95fb829460157
lennhodes-debug <lennhodes@icloud.com>
2026-02-11 20:22:20 +0100

    SEO: robots.txt uitgebreid met AI crawler blokkade, verlanglijst disallow en host directive

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,11 +2,22 @@ import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: ['/api/', '/_next/', '/afrekenen/', '/winkelwagen/'],
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/api/', '/_next/', '/afrekenen/', '/winkelwagen/', '/verlanglijst/'],
+      },
+      {
+        userAgent: 'GPTBot',
+        disallow: ['/'],
+      },
+      {
+        userAgent: 'CCBot',
+        disallow: ['/'],
+      },
+    ],
     sitemap: 'https://gameshopenter.nl/sitemap.xml',
+    host: 'https://gameshopenter.nl',
   };
 }
